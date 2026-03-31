2c143d7c7cb350ded772236fa0a19f2b971712d6
unknown <akunset20@gmail.com>
2026-03-31 10:16:22 +0700

    First commit

---
+++ modul5_103082400035/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_103082400035
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Berikut adalah pesan pertama");
+        }
+    }
+}
